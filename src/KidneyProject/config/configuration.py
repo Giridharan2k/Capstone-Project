d8314179c213dfe667f25d4539efd7fcf029d7ef
Giridharan2k <giridharanv16@gmail.com>
2024-03-01 16:32:45 +0530

    Changes added

--- src/KidneyProject/config/configuration.py
+++ src/KidneyProject/config/configuration.py
@@ -5,10 +5,10 @@ from src.KidneyProject.entity.config_entity import (DataIngestionConfig, Prepare
 
 class ConfigurationManager:
     def __init__(
-        self, config_filepath=CONFIG_FILE_PATH, params_fiepath=PARAMS_FILE_PATH
+        self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH
     ):
         self.config = read_yaml(config_filepath)
-        self.params = read_yaml(params_fiepath)
+        self.params = read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
 
